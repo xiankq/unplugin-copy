1c9310553933e3e78b4969b92aaddcfd059bdac9
xiankq <xiankq@qq.com>
2025-05-29 11:54:15 +0800

    refactor(buildFactory): 简化文件复制插件的实现
    - 移除了对多种构建工具的冗余支持，包括 Vite、Farm、Rollup、Esbuild、Webpack 和 Rspack
    - 删除了不必要的路径处理函数和配置解析逻辑
    - 优化了文件复制过程，使用 emitFile 方法直接处理文件
    - 精简了代码结构，提高了可维护性

--- src/core/buildFactory.ts
+++ src/core/buildFactory.ts
@@ -1,62 +1,29 @@
 import type { UnpluginFactory } from 'unplugin'
 import type { UnpluginCopyOptions } from '../types'
-import path from 'node:path/posix'
 import fs from 'fs-extra'
 import { resolveCopyTarget } from './resolve'
-import { normalizePosixPath } from './utils'
 
 const PLUGIN_NAME = 'unplugin-copy:build'
 export const buildFactory: UnpluginFactory<UnpluginCopyOptions, false> = (options, _meta) => {
   let called = false
-  let outputPath = ''
   return {
-    name: 'unplugin-copy:build',
+    name: PLUGIN_NAME,
     vite: {
       apply: 'build',
-      configResolved(config) {
-        outputPath = config.build.outDir
-      },
     },
-    farm: {
-      configResolved(config) {
-        outputPath = config.compilation?.output?.path || outputPath
-      },
-    },
-    // In the Rollup configuration, output.dir is used to store chunk, which is not the actual outDir. It can be directly output in the root directory.
-    rollup: {},
-    rolldown: {},
-    esbuild: {
-      setup(build) {
-        const { outdir } = build.initialOptions
-        outputPath = outdir || outputPath
-      },
-    },
-    webpack(compiler) {
-      compiler.hooks.emit.tap(PLUGIN_NAME, () => {
-        outputPath = compiler.options.output.path || outputPath
-      })
-    },
-    rspack(compiler) {
-      compiler.hooks.emit.tap(PLUGIN_NAME, () => {
-        outputPath = compiler.options.output.path || outputPath
-      })
-    },
-
     async buildEnd() {
       if (called) {
         return
       }
       called = true
-      const _outputPath = path.resolve(outputPath)
       const results = await resolveCopyTarget(options.targets)
       const resolves = results.map(e => e.resolves).flat()
       const promises = resolves.map(async (resolve) => {
-        const dest = normalizePosixPath(path.join(_outputPath, resolve.dest))
-        await fs.copy(resolve.src, dest)
         this.emitFile({
           type: 'asset',
           fileName: resolve.dest,
           source: await fs.readFile(resolve.src),
+          originalFileName: resolve.src,
         })
       })
       await Promise.all(promises)
